829f3f39a59f88074821abab2c983f6923c74803
AarohiBarua <73633463+AarohiBarua@users.noreply.github.com>
2020-12-24 12:40:51 +0530

    Add files via upload

--- Ball.js
+++ Ball.js
@@ -1,14 +1,20 @@
 class Ball{
-    constructor(x,y,width,height){
-        this.x = x;
-        this.y = y;
-        this.height = width;
-        this.width = height;
+    constructor(x,y,width,height,angle){
+        var options = {
+            density:1,
+            frictionAir:0.005
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+        World.add(world,this.body);
     }
     display(){
-       
-        this.body.position.x = mouseX;
-        this.body.position.y = mouseY;
-        super.display();
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x,this.body.position.y);
+        rotate(angle);
+        ellipse(0,0,this.width,this.height);
+        pop();
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,37 +1,42 @@
-
 const Engine = Matter.Engine;
-const World = Matter.World;
+const World= Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
 const Constraint = Matter.Constraint;
 
-var engine,world;
-var chain,ball;
+var engine, world;
+var ball1,ball2;
+var ground;
+var rope1,rope2;
 
-function setup() {
-	var canvas = createCanvas(1200, 400);
+function setup(){
+    var canvas = createCanvas(3000,800);
     engine = Engine.create();
-	world = engine.world;
+    world = engine.world;
+    ball1 = new Ball(200,200,80,80);
+    ball2 = new Ball(400,200,80,80);
+    ground = new Ground(600,600,1200,20);
+    rope1 = new Rope(ball1.body,{x:500,y:50});
+    rope2 = new Rope(ball2.body,{x:700,y:50});
+}
 
-	ball = new Ball(600,200,40,40);
-	chain = new Chain(pointA);
+function draw(){
+    background(255);
+    Engine.update(engine);
+    ball1.display();
+    ball2.display();
+    ground.display();
+    rope1.display();
+    rope2.display();
+}
 
-	Engine.run(engine);
-  
+function mouseDragged(){
+    Matter.Body.setPosition(ball1.body,{x:mouseX,y:mouseY});
+    Matter.Body.setPosition(ball2.body,{x:mouseX,y:mouseY});
 }
 
 
-function draw() {
-  background(0);
-  Engine.update(engine);
-  ball.display();
-  chain.display();
-  
 
-  
-  drawSprites();
+
+
  
-}
-
-
-
+ 
